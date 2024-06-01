eabd75440b9d7903c415839b3b27babf8658b08e
SarahFrainer <sbfrainer@gmail.com>
2024-05-31 22:11:30 -0300

    primeiraparte

--- src/classes/LivroCrud.js
+++ src/classes/LivroCrud.js
@@ -1,7 +1,43 @@
+const fs = require('fs');
+
+
 class LivroCrud {
 
     constructor() {
         this.filePath = './src/files/livros.json';
+    }
+
+    criar(livro) {
+
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+
+        conteudoAtual.push({
+            nome: livro.getNome,
+            autor: livro.getAutor,
+            genero: livro.getGenero,
+        })
+
+        console.log(livro);
+        fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoAtual),
+            'utf-8'
+        )
+    }
+
+    consultar(palavra) {
+        const conteudoAtual = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+
+        const encontrado = conteudoAtual.find(livro => livro.nome === palavra);
+
+        if (encontrado) {
+            console.log(encontrado);
+        }
+
+        else {
+            console.log ("Livro não encontrado!")
+        }
+
     }
 
 }
--- src/livros.js
+++ src/livros.js
@@ -1,4 +1,6 @@
 const readline = require('readline/promises');
+const Livro = require('./classes/Livro');
+const LivroCrud = require('./classes/LivroCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,15 +13,36 @@ async function run() {
 
     switch (resposta) {
         case 'criar':
-            /* Coloque sua resposta aqui  */
+            const nome = await rl.question("Qual o nome do livro?");
+            const autor = await rl.question("Qual o nome do autor?");
+            const paginas = await rl.question("Qual a quantidade de paginas?");
+            const genero = await rl.question("Qual o genero do livro?");
+
+            const livro = new Livro (nome, autor, paginas);
+            livro.setGenero = genero;
+
+            const crud = new LivroCrud();
+            crud.criar(livro);
+
+
+
             rl.close();
+
+
+            /* Coloque sua resposta aqui */
+            
+
             break;
         case 'deletar': {
-            /* Coloque sua resposta aqui */
+            /* Coloque sua resposta aqui - filter */
             rl.close();
             break;
         }
         case 'consultar': {
+            const palavra = await rl.question("Qual o nome do livro?");
+
+            const crud = new LivroCrud(palavra);
+            crud.consultar(palavra);
             /* Coloque sua resposta aqui */
             rl.close();
             break;

b4a89a9069c6619c718565b9b060afa0fe479f11
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-06-03 14:42:08 -0300

    feat(leitores): funções criar, consultar e deletar

--- src/leitores.js
+++ src/leitores.js
@@ -1,8 +1,11 @@
 const readline = require('readline/promises');
+const Leitor = require('./classes/Leitor');
+const LeitorCrud = require('./classes/LeitorCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    terminal: false
 });
 
 async function run() {
@@ -11,19 +14,45 @@ async function run() {
 
     switch (resposta) {
         case 'criar':
-            /* Coloque sua resposta aqui  */
+            const nomeLeitor = await rl.question('Informe o nome do leitor: ')
+
+            const leitor = new Leitor(nomeLeitor)  //inicializa a classe
+
+            const rgLeitor = await rl.question('Informe o rg do leitor: ')
+            leitor.setRg = rgLeitor
+
+            const cpfLeitor = await rl.question('Informe o cpf do leitor: ')
+            leitor.setCpf = cpfLeitor
+
+            const dataNasc = await rl.question('Informe a data de nascimento: ')
+            leitor.setDataNascimento = dataNasc
+
+            console.log("Leitor criado com sucesso!")
+
+            const crud = new LeitorCrud()
+            crud.criar(leitor)
+
             rl.close();
             break;
+
         case 'deletar': {
-            /* Coloque sua resposta aqui */
+            const palavra2 = await rl.question('Informe o nome do leitor que deseja deletar: ')
+            const crud = new LeitorCrud()
+            crud.deletar(palavra2)
+
             rl.close();
             break;
         }
+
         case 'consultar': {
-            /* Coloque sua resposta aqui */
+            const palavra = await rl.question('Informe o nome do leitor que deseja buscar: ')
+            const crud = new LeitorCrud()
+            crud.consultar(palavra)
+
             rl.close();
             break;
         }
+
         default:
             console.log("Ação não reconhecida.");
             rl.close();

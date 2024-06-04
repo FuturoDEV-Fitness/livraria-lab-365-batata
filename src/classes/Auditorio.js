b196af5ab318820254e63f83e5590c3ff8ef8612
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-06-04 10:27:07 -0300

    Exercicio finalizado

--- src/classes/Auditorio.js
+++ src/classes/Auditorio.js
@@ -1,8 +1,52 @@
-class Auditorio {
+const crypto = require('crypto');
 
-    constructor() {
+class Auditorio {
+    #codigo = 0;
+    #nome = "";
+    #descricao = "";
+    #capacidade = 0;
+
+    constructor(nome) {
+        this.nome = nome;
+        this.codigo = crypto.randomUUID();
 
     }
+
+    get codigo() {
+        return this.#codigo;
+    }
+
+    set codigo(codigo) {
+        this.#codigo = codigo;
+    }
+    get nome() {
+        return this.#nome;
+    }
+    set nome(nome) {
+        this.#nome = nome;
+    }
+    get descricao() {
+        return this.#descricao;
+    }
+    set descricao(descricao) {
+        this.#descricao = descricao;
+    }
+    get capacidade() {
+        return this.#capacidade;
+    }
+    set capacidade(capacidade) {
+        this.#capacidade = capacidade;
+    }
+    toString() {
+        return `Auditorio {
+            Codigo: ${this.#codigo},
+            Nome: ${this.#nome},
+            Descricao: ${this.#descricao},
+            Capacidade: ${this.#capacidade}
+        }`;
+    }
+
+
 
 }
 
--- src/classes/AuditorioCrud.js
+++ src/classes/AuditorioCrud.js
@@ -1,9 +1,75 @@
+const fs = require('fs');
+
 class AuditorioCrud {
-    
+    // #filePath = './src/files/auditorios.json';
     constructor() {
+
         this.filePath = './src/files/auditorios.json';
+
+    }
+    criar(auditorio) {
+        const conteudo = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+        conteudo.push({
+            nome: auditorio.nome,
+            descricao: auditorio.descricao,
+            capacidade: auditorio.capacidade,
+            codigo: auditorio.codigo
+        });
+        fs.writeFileSync(this.filePath,
+            JSON.stringify(conteudo, null, 2),
+            'utf-8');
+        console.log('Auditorio criado com sucesso');
     }
 
+    consultar(auditorio) {
+        const conteudo = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+        const resposta = conteudo.find((item) => item.nome === auditorio);
+        if (resposta) {
+            return console.log(resposta);
+        }
+        return console.log('Auditorio não encontrado');
+    }
+
+    deletar(auditorio) {
+        const conteudo = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+        const auditórioEncontrado = conteudo.find((item) => item.nome === auditorio);
+        console.log(auditórioEncontrado)
+        if (!auditórioEncontrado) {
+            return console.log('Auditório não encontrado.');
+        } else {
+            const resposta = conteudo.filter((item) => item.nome !== auditorio);
+            fs.writeFileSync(this.filePath,
+                JSON.stringify(resposta, null, 2),
+                'utf-8');
+            return console.log('Auditorio deletado com sucesso');
+        }
+    }
+
+    alterar(nomeAntigo, nomeNovo, descricao, capacidade) {
+        const conteudo = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+        let auditórioEncontrado = false;
+        const resposta = conteudo.map((item) => {
+            if (item.nome === nomeAntigo) {
+                auditórioEncontrado = true;
+                return {
+                    nome: nomeNovo,
+                    descricao: descricao,
+                    capacidade: capacidade,
+                    codigo: item.codigo
+                }
+            }
+            return item;
+        });
+        if (auditórioEncontrado) {
+            console.log('Auditorio alterado com sucesso');
+            fs.writeFileSync(this.filePath,
+                JSON.stringify(resposta, null, 2),
+                'utf-8');
+        } else {
+            return console.log('Auditorio não encontrado');
+        }
+
+    }
 }
 
 module.exports = AuditorioCrud;
